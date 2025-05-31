e61dc121a7d31baff92c0942ff0edde50b341fe7
Leonardo Marqueti <dommessi2@gmail.com>
2025-05-31 18:30:49 +0000

    Error: In HTML, whitespace text nodes cannot be a child of <html>. Make sure you don't have any extra whitespace between tags on each line of your source code. This will cause a hydration error.
      ...
        <HotReload assetPrefix="" globalError={[...]}>
          <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
            <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
              <ReplaySsrOnlyErrors>
              <DevRootHTTPAccessFallbackBoundary>
                <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
                  <HTTPAccessFallbackErrorBoundary pathname="/desktop" notFound={<NotAllowedRootHTTPFallbackError>} ...>
                    <RedirectBoundary>
                      <RedirectErrorBoundary router={{...}}>
                        <Head>
                        <link>
                        <script>
                        <script>
                        <script>
                        <RootLayout>
    >                     <html lang="pt-BR" className="dark">
    >                       {" "}
                            ...
                        ...
            ...
    
        at createUnhandledError (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:879:71)
        at handleClientError (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:1052:56)
        at console.error (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:1191:56)
        at validateTextNesting (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:4071:174)
        at completeWork (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:9585:87)
        at runWithFiberInDEV (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:3501:74)
        at completeUnitOfWork (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:10233:23)
        at performUnitOfWork (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:10170:28)
        at workLoopSync (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:10060:40)
        at renderRootSync (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:10043:13)
        at performWorkOnRoot (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:9784:212)
        at performWorkOnRootViaSchedulerTask (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:10796:9)
        at MessagePort.performWorkUntilDeadline (https://6000-firebase-studio-1748702455778.cluster-ve345ymguzcd6qqzuko2qbxtfe.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js:1952:64)
        at html (<anonymous>)
        at RootLayout (rsc://React/Server/file:///home/user/studio/.next/server/chunks/ssr/_796d4ab7._.js?6:60:263)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,7 @@ export default function RootLayout({
   children,
 }: RootLayoutProps) {
   return (
-    <html lang="pt-BR" className="dark">
-      <head>
+    <html lang="pt-BR" className="dark"><head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
